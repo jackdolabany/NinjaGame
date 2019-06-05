b6b3729d878879094cc8ca92794590a0ea849ccf
Jason Skenderis <jason_skenderis@hotmail.com>
2019-06-04 21:51:34 -0400

    Added Tileset1 to ninjagame folder

---
+++ NinjaGame/Content/Maps/Tileset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="Platform Tileset 4" tilewidth="16" tileheight="16" tilecount="576" columns="32">
+ <image source="Platform Tileset 4.png" width="512" height="300"/>
+</tileset>
